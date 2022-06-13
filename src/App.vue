b29d0e19ec6aae47736a46360aadd2da674635d1
ZnoGouDj <znogoud3@gmail.com>
2022-06-13 19:56:15 +0200

    Basic form funcitonality

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
+  <div class="app">
     <div>
-      LIKES! <strong>{{ likes }}</strong>
+      <form @submit.prevent>
+        <h4>Post creation</h4>
+        <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Description"
+        />
+        <button class="btn" @click="createPost">Create</button>
+      </form>
     </div>
-    <div>
-      DISLIKES! <strong>{{ dislikes }}</strong>
-    </div>
-    <div>
-      <button @click="addLike">Like</button>
-      <button @click="addDislike">Dislike</button>
+    <div class="post" v-for="post in posts" :key="post.title">
+      <div><strong>Name:</strong> {{ post.title }}</div>
+      <div><strong>Name:</strong> {{ post.body }}</div>
     </div>
   </div>
 </template>
@@ -17,21 +31,69 @@
 export default {
   data() {
     return {
-      likes: 5,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: 'JavaScript', body: 'An article description' },
+        { id: 2, title: 'JavaScript 2', body: 'An article description 2' },
+        { id: 3, title: 'JavaScript 3', body: 'An article description 3' },
+        { id: 3, title: 'JavaScript 4', body: 'An article description 4' },
+      ],
+      title: '',
+      body: '',
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
     },
   },
 };
 </script>
 
-<style></style>
+"scoped" -- local styles👇
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-// Single file component
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
+
+// Single file component | |
