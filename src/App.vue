e93af8485da3f593fcc33a83464d1f67561da103
ZnoGouDj <znogoud3@gmail.com>
2022-06-13 23:04:10 +0200

    delete post

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <post-form @create="createPost" />
-    <post-list :posts="posts" />
+    <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
 
@@ -20,13 +20,16 @@ export default {
         { id: 1, title: 'JavaScript', body: 'An article description' },
         { id: 2, title: 'JavaScript 2', body: 'An article description 2' },
         { id: 3, title: 'JavaScript 3', body: 'An article description 3' },
-        { id: 3, title: 'JavaScript 4', body: 'An article description 4' },
+        { id: 4, title: 'JavaScript 4', body: 'An article description 4' },
       ],
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id);
     },
   },
 };
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>User list</h3>
-    <post-item v-for="post in posts" :key="post.title" :post="post" />
+    <post-item v-for="post in posts" :key="post.id" :post="post" @remove="$emit('remove', post)" />
   </div>
 </template>
 
