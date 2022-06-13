8fbeb94d5c07ff388b27e592bd46b5df22aaa347
ZnoGouDj <znogoud3@gmail.com>
2022-06-13 20:11:56 +0200

    Components split, props passing, v-model

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <template>
   <div class="app">
-    <div>
-      <form @submit.prevent>
-        <h4>Post creation</h4>
-        <input
-          v-bind:value="title"
-          @input="title = $event.target.value"
-          class="input"
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          v-bind:value="body"
-          @input="body = $event.target.value"
-          class="input"
-          type="text"
-          placeholder="Description"
-        />
-        <button class="btn" @click="createPost">Create</button>
-      </form>
-    </div>
-    <div class="post" v-for="post in posts" :key="post.title">
-      <div><strong>Name:</strong> {{ post.title }}</div>
-      <div><strong>Name:</strong> {{ post.body }}</div>
-    </div>
+    <post-form />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from './components/PostForm';
+import PostList from '@/components/PostList';
+
 export default {
+  components: {
+    PostList,
+    PostForm,
+  },
   data() {
     return {
       posts: [
@@ -37,21 +22,10 @@ export default {
         { id: 3, title: 'JavaScript 3', body: 'An article description 3' },
         { id: 3, title: 'JavaScript 4', body: 'An article description 4' },
       ],
-      title: '',
-      body: '',
     };
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body,
-      };
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
-    },
+    createPost() {},
   },
 };
 </script>
@@ -64,35 +38,8 @@ export default {
   box-sizing: border-box;
 }
 
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-
 .app {
   padding: 20px;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
 
---
+++ src/components/PostForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <form @submit.prevent>
+    <h4>Post creation</h4>
+    // two-way binding -- two types below: v-model || v-bind + @input
+    <input v-model="post.title" class="input" type="text" placeholder="Name" />
+    <input
+      v-bind:value="post.body"
+      @input="post.body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Description"
+    />
+    <button class="btn" @click="createPost">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
+};
+</script>
+
+<style>
+.input {
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
