d915cc2d990f80284ce88ff9b143aa92da6186f9
ZnoGouDj <znogoud3@gmail.com>
2022-06-15 11:15:06 +0200

    VUE ROUTER

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Vue 3</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Posts</my-button>
+      <my-button style="margin-left: 20px" @click="$router.push('/about')">About</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  background-color: lightgray;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+
+  div:first-child {
+    cursor: pointer;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: yellowgreen;
+  }
+
+  .navbar__btns {
+    margin-left: auto;
+  }
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><h1>App to learn Vue</h1></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
